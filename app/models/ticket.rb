a0c1dbc5b25a1a403de3d9bae20ca3c2c8f8473d
Jason Shaw <shawjaso@uw.edu>
2012-03-08 22:56:32 -0800

    Add email ticket notifications

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -15,6 +15,9 @@ class Ticket < ActiveRecord::Base
   has_many :comments
 
   has_and_belongs_to_many :tags
+  has_and_belongs_to_many :watchers, :join_table => "ticket_watchers",
+                                     :class_name => "User"
+  after_create :creator_watches_me
 
   def tag!(tags)
     tags = tags.split(" ").map do |tag|
@@ -22,5 +25,10 @@ class Ticket < ActiveRecord::Base
     end
     self.tags << tags
   end
+
+  private
+    def creator_watches_me
+      self.watchers << user
+    end
 end
 
